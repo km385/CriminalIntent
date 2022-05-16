d2e4757aa29097d0e69b7333184ab70a2295e8f3
Jakub <jakub@pop-os.localdomain>
2022-05-16 16:54:18 +0200

    created new repo

---
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -0,0 +1,17 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
