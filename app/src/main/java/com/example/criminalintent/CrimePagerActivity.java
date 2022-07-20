838c53fe6ffa0acacd936a740e07d910682431ae
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-07-20 14:50:36 +0200

    got rid of unnecessary listener

--- app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -59,23 +59,6 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
         
     }
 }
