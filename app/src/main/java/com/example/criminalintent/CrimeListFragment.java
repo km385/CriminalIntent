4ea8e9cd946843f72c82ce4cee3244f4e83f9eae
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-07-30 16:59:53 +0200

    added ability to refresh crime list upon clicking back button

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -164,6 +164,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         }else{
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
             //TODO make it using ActivityResultLauncher
             //mAdapter.notifyItemChanged(lastClicked);
@@ -278,6 +279,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes){
+            mCrimes = crimes;
         }
 
         @Override
